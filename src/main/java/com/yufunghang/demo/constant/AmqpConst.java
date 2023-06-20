694480a040d982577db9a1e88683d83f4c657cd9
御風行 <1587898817@qq.com>
2023-06-20 16:51:09 -0400

    AmqpConst

---
+++ src/main/java/com/yufunghang/demo/constant/AmqpConst.java
@@ -0,0 +1,9 @@
+package com.yufunghang.demo.constant;
+
+public class AmqpConst {
+    public static final String DEMO_EXCHANGE = "demo.exchange";
+
+    public static final String DEMO_QUEUE = "demo.queue";
+
+    public static final String DEMO_ROUTING_KEY = "demo.routing";
+}
